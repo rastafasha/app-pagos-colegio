28a1ff89fc0a5c48e33f4b50b453f6dbbb1ae7b6
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-06-01 17:35:57 -0400

    fix: Remove unnecessary blank lines in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,6 @@
 
 export const environment = {
   production: false,
-
-
   // url_backend: 'http://127.0.0.1:8000/',
   //   url_servicios: 'http://127.0.0.1:8000/api',
   //   url_frontend: 'http://localhost:4200/',
